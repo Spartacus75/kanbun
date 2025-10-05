be6487e7da47b7177fd3a593db87aea563ccbfdd
Spartacus75 <maanan.seduk@gmail.com>
2025-10-05 21:31:13 +0200

    build ok + pixel tiktok

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { i18n, type Locale } from '@/i18n/config';
 import "../globals.css";
 
@@ -103,6 +104,23 @@ export default async function LangLayout(props: {
   return (
     <html lang={lang}>
       <body className="antialiased">
+        {/* TikTok Pixel Code */}
+        <Script
+          id="tiktok-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+  ttq.load('D3HC6IRC77UCTDLH2E3G');
+  ttq.page();
+}(window, document, 'ttq');
+            `,
+          }}
+        />
         {props.children}
       </body>
     </html>
